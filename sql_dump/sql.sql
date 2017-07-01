8b863b8c88707a89acb5c89e8ca3828e09fd95be
Timur Zevriev <timur.z@wecandevelopit.com>
2017-07-01 16:48:42 +0300

    sdf

--- sql_dump/sql.sql
+++ sql_dump/sql.sql
@@ -1,16 +1,47 @@
+/*
+SQLyog Ultimate v12.4.1 (64 bit)
+MySQL - 5.7.18-15 : Database - events
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`events` /*!40100 DEFAULT CHARACTER SET ascii */;
+
+USE `events`;
+
+/*Table structure for table `counter` */
+
+DROP TABLE IF EXISTS `counter`;
+
 CREATE TABLE `counter` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
-  `state` char(2) DEFAULT NULL,
+  `country` char(2) DEFAULT NULL,
   `action` smallint(2) DEFAULT NULL,
-  `counter` int(11) DEFAULT '0',
+  `counter` int(11) DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `data` (`state`,`action`,`date`)
-) ENGINE=MyISAM DEFAULT CHARSET=ascii
+  UNIQUE KEY `data` (`country`,`action`,`date`)
+) ENGINE=MyISAM AUTO_INCREMENT=3880408 DEFAULT CHARSET=ascii;
+
+/*Table structure for table `countries` */
+
+DROP TABLE IF EXISTS `countries`;
 
 CREATE TABLE `countries` (
   `id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `code` char(2) NOT NULL,
   `title` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=246 DEFAULT CHARSET=ascii
+) ENGINE=MyISAM AUTO_INCREMENT=246 DEFAULT CHARSET=ascii;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

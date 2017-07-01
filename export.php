b4c653bde4a8b0f9aa963937aa8aac56095e1998
Timur Zevriev <timur.z@wecandevelopit.com>
2017-07-01 16:15:35 +0300

    sdf

--- export.php
+++ export.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             return_csv($db);
             break;
         default:
-            echo "Please select report format"
+            echo "Please select report format"ж
             break;
     }
 }
